defdacc60027ca3eae705985ec175f90bab33032
ignous <lizhygm@sina.com>
2016-10-15 14:38:50 +0800

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'index.js',
+  plugins: [babel({
+    "presets": [
+      [
+        "es2015",
+        {
+          "modules": false
+        }
+      ]
+    ],
+    "plugins": [
+      "external-helpers"
+    ]
+  })],
+  format: 'umd',
+  moduleName: 'jFactory',
+  dest: 'dist/jFactory-umd.js'
+};
